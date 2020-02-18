84772e0304710d139d8d99b9da2fb7599bccb342
John Hodson <hodsonus@gmail.com>
2020-02-18 17:39:48 -0500

    update for x86 - was broken before

--- tags/tags.c
+++ tags/tags.c
@@ -2,8 +2,8 @@
 #include <unistd.h>
 #include <sys/syscall.h>
 
-#define __NR_get_tag 295
-#define __NR_set_tag 296
+#define __NR_get_tag 335
+#define __NR_set_tag 336
 
 int set_tag(int pid, int new_tag) {
     return syscall(__NR_set_tag, pid, new_tag);
