cebddbac102545aeeae2c2ceb1f573ffc2d5d402
John Hodson <hodsonus@gmail.com>
2020-03-29 15:24:45 -0400

    reverse direction of initialization

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6041,7 +6041,7 @@ void __init sched_init(void)
 
 		levels_rq = cpu_levels_rq(i);
 
-		for (level = 0; level < NUM_TASK_LEVELS; ++level) {
+		for (level = NUM_TASK_LEVELS-1; level >= 0; --level) {
 			struct rq *rq;
 
 			rq = &levels_rq->rqs[level];
