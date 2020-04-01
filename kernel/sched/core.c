2407811939499aba7a5abf092b25d47738587e29
John Hodson <hodsonus@gmail.com>
2020-04-01 00:06:36 -0400

    fix idle initialization

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5533,48 +5533,55 @@ void show_state_filter(unsigned long state_filter)
  */
 void init_idle(struct task_struct *idle, int cpu)
 {
-	struct rq *rq = cpu_rq(cpu);
+	int level;
+	struct levels_rq *levels_rq = cpu_levels_rq(cpu);
 	unsigned long flags;
+	struct rq *rq;
 
 	raw_spin_lock_irqsave(&idle->pi_lock, flags);
-	raw_spin_lock(&rq->lock);
 
-	__sched_fork(0, idle);
-	idle->state = TASK_RUNNING;
-	idle->se.exec_start = sched_clock();
-	idle->flags |= PF_IDLE;
+	for (level = 0; level < NUM_TASK_LEVELS; ++level)
+	{
+		rq = &levels_rq->rqs[level];
+		raw_spin_lock(&rq->lock);
 
-	kasan_unpoison_task_stack(idle);
+		__sched_fork(0, idle);
+		idle->state = TASK_RUNNING;
+		idle->se.exec_start = sched_clock();
+		idle->flags |= PF_IDLE;
 
-#ifdef CONFIG_SMP
-	/*
-	 * Its possible that init_idle() gets called multiple times on a task,
-	 * in that case do_set_cpus_allowed() will not do the right thing.
-	 *
-	 * And since this is boot we can forgo the serialization.
-	 */
-	set_cpus_allowed_common(idle, cpumask_of(cpu));
-#endif
-	/*
-	 * We're having a chicken and egg problem, even though we are
-	 * holding rq->lock, the CPU isn't yet set to this CPU so the
-	 * lockdep check in task_group() will fail.
-	 *
-	 * Similar case to sched_fork(). / Alternatively we could
-	 * use task_rq_lock() here and obtain the other rq->lock.
-	 *
-	 * Silence PROVE_RCU
-	 */
-	rcu_read_lock();
-	__set_task_cpu(idle, cpu);
-	rcu_read_unlock();
+		kasan_unpoison_task_stack(idle);
 
-	rq->curr = rq->idle = idle;
-	idle->on_rq = TASK_ON_RQ_QUEUED;
-#ifdef CONFIG_SMP
-	idle->on_cpu = 1;
-#endif
-	raw_spin_unlock(&rq->lock);
+	#ifdef CONFIG_SMP
+		/*
+		 * Its possible that init_idle() gets called multiple times on a task,
+		 * in that case do_set_cpus_allowed() will not do the right thing.
+		 *
+		 * And since this is boot we can forgo the serialization.
+		 */
+		set_cpus_allowed_common(idle, cpumask_of(cpu));
+	#endif
+		/*
+		 * We're having a chicken and egg problem, even though we are
+		 * holding rq->lock, the CPU isn't yet set to this CPU so the
+		 * lockdep check in task_group() will fail.
+		 *
+		 * Similar case to sched_fork(). / Alternatively we could
+		 * use task_rq_lock() here and obtain the other rq->lock.
+		 *
+		 * Silence PROVE_RCU
+		 */
+		rcu_read_lock();
+		__set_task_cpu(idle, cpu);
+		rcu_read_unlock();
+
+		rq->curr = rq->idle = idle;
+		idle->on_rq = TASK_ON_RQ_QUEUED;
+	#ifdef CONFIG_SMP
+		idle->on_cpu = 1;
+	#endif
+		raw_spin_unlock(&rq->lock);
+	}
 	raw_spin_unlock_irqrestore(&idle->pi_lock, flags);
 
 	/* Set the preempt count _outside_ the spinlocks! */
